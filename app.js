6bb9de035772d1626785bf5b6f15760cb989c616
ogwk <kanakkuma.0528@gmail.com>
2021-02-24 20:39:28 +0900

    ログインエラー

--- app.js
+++ app.js
@@ -32,12 +32,29 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//DB接続
+var {Client} = require('pg'); 
 
 
 var LocalStrategy = require('passport-local').Strategy;
 
+//ログインチェック
 passport.use(new LocalStrategy(function(username, password, done)
+{
+    //ここを通らない
+    console.log(username);　
+    console.log(password);
+    if (username === "test" && password === "test") {
+        return done(null, username)
+      } else {
+        console.log("login error")
+        return done(null, false, { message: 'パスワードが正しくありません。' })
+      }
+    })
+);
+/*
     {
+        
         console.log("test");
         User.findOne({ where: { username: [username] } },function(err, user)
         { //
@@ -56,9 +73,34 @@ passport.use(new LocalStrategy(function(username, password, done)
             console.log("test4");
             return done(null, user);
         });
-    })
-);
+    }
+*/
+/*
+{
+    var client = new Client({
+        user: 'postgres',
+        host: 'localhost',
+        database: 'users',
+        password:'postgres',
+        port:8000
+    });
+    const query = 'select * from users where users.name =' + username;
+    client.query(query, function(err, res) {
+        console.log(query);
+        console.log(res);
+        if(err){
 
+        }
+        if(res.password === password){
+
+        }else{
+
+        }
+    })
+}
+));
+*/
+/*
 passport.serializeUser(function(user, done) {
     done(null, user.id);
   });
@@ -68,7 +110,7 @@ passport.serializeUser(function(user, done) {
       done(err, user);
     });
   });
-
+*/
 //middleware
 /*
 app.use(session({
@@ -80,9 +122,9 @@ app.use(session({
     }
 }));
 */
+
 app.get('/login',(req, res, next)=>{
     let {login, error} = req.flash();
-    
     res.render('login', {error: error});
 });
 
@@ -102,6 +144,7 @@ app.post('/login',(req, res, next)=>{                         /* 横取り開始
         }                      /* フォームに入力があれば認証処理を呼ぶ */
     },
     passport.authenticate('local',{
+        
         successRedirect: '/',
         failureRedirect: '/login',
         failureFlash:    true
